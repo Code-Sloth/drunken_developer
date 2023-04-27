f5bbcb15c2176681b7c3c60520883c97eb01be2c
codesloth <djqmflsk@naver.com>
2023-04-27 16:28:19 +0900

    import

--- products/views.py
+++ products/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Product, Comment
+from .forms import ProductForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, get_list_or_404
 
 # Create your views here.
 
