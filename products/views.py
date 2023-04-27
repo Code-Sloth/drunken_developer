d58ea5fc820c22ee06c4f59aafeb247b627191dc
syyoung <yuyoung.dev@gmail.com>
2023-04-27 16:57:15 +0900

    index

--- products/views.py
+++ products/views.py
@@ -5,4 +5,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, get_list_or_404
 
 # Create your views here.
-
+def index(request):
+    products = get_list_or_404(Product)
+    context = {
+        'products' : products,
+    }
+    return render(request, 'products/index.html',context)
