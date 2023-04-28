40bb181ac22cdb73559a8fb54f67d29f228b5587
SunHyeok <cghhld@naver.com>
2023-04-28 14:37:45 +0900

    re

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path('profile/<user_pk>', views.profile, name='profiel'),
+    path('profile/<user_pk>', views.profile, name='profile'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
