5d4d6c9deee823f4d2e8d0ff64713a9033c75c2f
codesloth <djqmflsk@naver.com>
2023-05-02 11:32:54 +0900

    fixtures

--- aaproject/urls.py
+++ aaproject/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('products/', include('products.urls')),
     path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
