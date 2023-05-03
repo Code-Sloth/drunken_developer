c574a04e07bcc878831a3cae88de34fa309e8105
syyoung <yuyoung.dev@gmail.com>
2023-05-03 14:00:07 +0900

    mypage

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path('profile/<str:username>/', views.profile, name='profile'),
+    path('mypage/<str:username>/', views.mypage, name='mypage'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
@@ -12,4 +12,8 @@ urlpatterns = [
     path('update/', views.update, name='update'),
     path('password/', views.change_password, name='change_password'),
     path('<int:user_pk>/follow/', views.follow, name='follow'),
+    path('<str:username>/like/', views.like, name='like'),
+    path('<str:username>/review/', views.review, name='review'),
+    path('<str:username>/profile/', views.profile, name='profile'),
+    path('<str:username>/purchase/', views.purchase, name='purchase'),
 ]
